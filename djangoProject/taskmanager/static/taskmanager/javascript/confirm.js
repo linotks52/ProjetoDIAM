547184371d1f288362100c9c70a685316bfe51c0
linotks52 <dbrlo@iscte-iul.pt>
2023-05-13 19:24:32 +0100

    Commit

---
+++ djangoProject/taskmanager/static/taskmanager/javascript/confirm.js
@@ -0,0 +1,14 @@
+function confirmTask(){
+        if (confirm("Essa ação é irreversível, deseja prosseguir?")) {
+            window.location.href = "{% url 'taskmanager:delete_task' task.id %}";
+        }
+    }
+
+    function confirmUser(){
+        var myButton = document.getElementById("delete");
+    myButton.addEventListener("click", function() {
+        if (confirm("Essa ação é irreversível, deseja prosseguir?")) {
+            window.location.href = "{% url 'taskmanager:delete_user' utilizador.id %}";
+        }
+    });
+    }
